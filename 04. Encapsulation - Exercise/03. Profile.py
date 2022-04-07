1bf8bda371f204e318d8ce6e336fb88bfad93b4a
Samuil <samuil.pavlevchev@gmail.com>
2022-04-07 10:44:35 +0100

    Problems

---
+++ 04. Encapsulation - Exercise/03. Profile.py
@@ -0,0 +1,33 @@
+class Profile:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    @property
+    def username(self):
+        return self.__username
+
+    @username.setter
+    def username(self, value):
+        if 5 <= len(value) <= 15:
+            self.__username = value
+        else:
+            raise ValueError("The username must be between 5 and 15 characters.")
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        length = len(value) >= 8
+        capital = any([True for x in value if x.isupper()])
+        digits = any([True for x in value if x.isdigit()])
+
+        if length and capital and digits:
+            self.__password = value
+        else:
+            raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+
+    def __str__(self):
+        return f'You have a profile with username: "{self.username}" and password: {"*" * len(self.password)}'
