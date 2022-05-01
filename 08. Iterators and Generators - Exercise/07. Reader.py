9cd447ce51429a38a097d76e1590d97946894522
Samuil <samuil.pavlevchev@gmail.com>
2022-05-01 09:32:06 +0100

    Problems

---
+++ 08. Iterators and Generators - Exercise/07. Reader.py
@@ -0,0 +1,9 @@
+def read_next(*args):
+    for x in args:
+        for y in x:
+            yield y
+
+
+for item in read_next("string", (2,), {"d": 1, "I": 2, "c": 3, "t": 4}):
+    print(item, end='')
+
