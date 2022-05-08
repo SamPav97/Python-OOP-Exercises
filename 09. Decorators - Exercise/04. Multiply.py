2fcda5c367694668a30b51c2eea0ae8d155f880d
Samuil <samuil.pavlevchev@gmail.com>
2022-05-08 10:13:14 +0100

    Exercises

---
+++ 09. Decorators - Exercise/04. Multiply.py
@@ -0,0 +1,13 @@
+def multiply(times):
+    def decorator(function):
+        def wrapper(n):
+            function(n) * times
+            return function(n) * times
+        return wrapper
+    return decorator
+
+
+@multiply(3)
+def add_ten(number):
+    return number + 10
+print(add_ten(3))
